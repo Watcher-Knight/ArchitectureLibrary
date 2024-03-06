f43d7c201f0a46c8687f43b38fd1f700925eedb7
Elijah Pennington <elijahmpennington@gmail.com.com>
2024-03-06 15:41:43 -0600

    Update animator editor paramerers

--- Animation/AnimatorParameters.cs
+++ Animation/AnimatorParameters.cs
@@ -14,16 +14,34 @@ namespace ArchitectureLibrary
     public class AnimatorBoolParameter : AnimatorParameter
     {
         public void SetValue(bool value) => Animator.SetBool(Name, value);
+        public bool GetValue() => Animator.GetBool(Name);
+        public bool Value
+        {
+            get => GetValue();
+            set => SetValue(value);
+        }
     }
     [Serializable]
     public class AnimatorFloatParameter : AnimatorParameter
     {
         public void SetValue(float value) => Animator.SetFloat(Name, value);
+        public float GetValue() => Animator.GetFloat(Name);
+        public float Value
+        {
+            get => GetValue();
+            set => SetValue(value);
+        }
     }
     [Serializable]
     public class AnimatorIntParameter : AnimatorParameter
     {
         public void SetValue(int value) => Animator.SetInteger(Name, value);
+        public int GetValue() => Animator.GetInteger(Name);
+        public int Value
+        {
+            get => GetValue();
+            set => SetValue(value);
+        }
     }
     [Serializable]
     public class AnimatorTriggerParameter : AnimatorParameter
