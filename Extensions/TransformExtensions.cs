a8fecb9e3b8437d65b3f29dec6010c44237387d8
Elijah Pennington <elijahmpennington@gmail.com.com>
2024-03-06 15:36:34 -0600

    Fix transform flip method

--- Extensions/TransformExtensions.cs
+++ Extensions/TransformExtensions.cs
@@ -5,6 +5,6 @@ namespace ArchitectureLibrary
     public static class TransformExtensions
     {
         public static void Flip(this Transform transform) =>
-            transform.position = new Vector3(transform.position.x * -1, transform.position.y, transform.position.z);
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
